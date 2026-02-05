9aad7a6aced552f5b7dab5d618c0dcf40d4594cb
Matthew Thomas <matthew.a.thomas.uk@gmail.com>
2026-02-05 14:02:11 +0000

    covariance tapering functions

--- src/qktf.py
+++ src/qktf.py
@@ -1,11 +1,34 @@
 import cupy as np
+import numpy
+
+def cov_matern(d, loghyper, x):
+    ell = np.exp(loghyper[0])
+    sf2 = np.exp(2*loghyper[1])
+    def f(t):
+        if d == 1: return 1
+        if d == 3: return 1+t
+        if d == 5: return 1+t*(1+t/3)
+        if d == 7: return 1+t*(1+t*(6+t)/15)
+    def m(t):
+        return f(t)*np.exp(-t)
+    dist_sq = ((x[:, None] - x[None, :])/ell)**2
+    return sf2*m(np.sqrt(d*dist_sq))
+
+def bohman(loghyper, x):
+    range_ = np.exp(loghyper[0])
+    dius = np.abs(x[:, None] - x[None, :])
+    r = np.minimum(dis/range_, 1)
+    k = (1-r)*np.cos(np.pi*r)+np.sin(np.pi*r)/np.pi
+    k[k < 1e-16] = 0
+    k[np.isnan(k)] = 0 
+    return k
 
 def unfold(tensor, mode):
     """
     Function that performs a mode-d unfolding of a tensor.
     
-    :param tensor: Input tensor
-    :param mode: mode-d of unfolding
+    :param tensor: input tensor
+    :param mode: axis to do unfolding
     """
     return np.reshape(np.moveaxis(tensor, mode, 0), (tensor.shape[mode], -1), order = 'F')
 
@@ -13,13 +36,32 @@ def fold(mat, dim, mode):
     """
     Docstring for fold
     
-    :param mat: Description
-    :param dim: Description
-    :param mode: Description
+    :param mat: unfolded tensor
+    :param dim: tuple/1d array containing dimensions of original tensor
+    :param mode: axis where tensor was unfolded
     """
-    index = list()
-    index.append(mode)
-    for i in range(dim.shape[0]):
+    index = list() # defines new axis order - encodes which axes became rows
+    index.append(mode) # ensures 'mode' axis is first
+    for i in range(dim.shape[0]): # appends all other axes in order
         if i != mode:
             index.append(i)
     return np.moveaxis(np.reshape(mat, list(dim[index]), order = 'F'), 0, mode)
+
+def global_admm(u, lambda, rho, kdu, mask, tau, max_iter):
+    """
+    ADMM algorithm for estimating the global structure of data tensor
+    
+    :param u: latent matrices
+    :param lamda: regulariser
+    :param rho: regulariser
+    :param kdu: covariance matrix
+    :param mask: mask matrix
+    :param tau: quantile level
+    :param max_iter: maximum number of iterations
+    """
+    
+
+
+
+
+
