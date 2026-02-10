788cbc3a3a2db693e7374008589399b3df500ad0
Matthew Thomas <matthew.a.thomas.uk@gmail.com>
2026-02-10 09:19:49 +0000

    binary indicator matrix

--- src/qktf.py
+++ src/qktf.py
@@ -59,8 +59,34 @@ def global_admm(u, lambda, rho, kdu, mask, tau, max_iter):
     :param tau: quantile level
     :param max_iter: maximum number of iterations
     """
+    for k in range(max_iter):
+        # latent matrix U_d^(k+1) update
+        def shrink(x, alpha):
+            """Shrink[x, alpha] = sgn(x) * max(abs(x) - alpha, 0)
+            """
+            return np.sign(x) * np.maximum(np.abs(x) - alpha, 0)
+        
+    x, s, vh = np.linalg.svd(u) # svd to compute singular values for computation of eta (largest eigenvalues of u^T*u)
+    eta = s[0]**2 # s[0] is the largest singular value
 
+def qktf(X, mask_data):
+    N = X.shape # gets the shape of the data
+    N = numpy.array(N) # sets the shape of the data to an array
+    D = X.ndim # gets the dimensions of the data
     
+    mask_data = mask_data.astype(bool) # sets data to True if observed, False if not observed
+    non_obs = np.where(mask_data == 0) # variable for unobserved data
+    mask_matrix = [unfold(X, d) for d in range(D)] # unfolds binary matrix
+    col = np.sum(mask_matrix[2], axis = 0) > 0 # returns boolean mask where columns of mask_matrix contain at least one zero
+    train_mat = X * mask_data # sets only observed data in actual dataset
+    mask = train_mat > 0 # creates boolean mask of same shape as train_mat
+    train_mat = train_mat[mask] # whenever mask is True keep data point, if False discard - creates 1-D array of positive entries
+    X_centred = X - np.mean(train_mat) # removes global mean before modelling - ADMM converges faster
+    obs_centred = X_centred * mask_data # keeps mean-centred values for observed data
+    mask_matrix_c = [mask_matrix[d].T for d in range(D)] # creates O_(d)^T
+    vec_mask = [mask_matrix[d].ravel(order = 'F') for d in range(D)] # creates vec(O_(d)^T)
+    obs = [np.where(mask_flat[d] == 1) for d in range(D)] # where data is observed in vec(O_(d)^T)
+
 
 
 
